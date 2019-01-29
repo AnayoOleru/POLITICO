93ed727fac0429e50d1993a452f442ded5531b83
Anayo Oleru <anayo_oleru@outlook.com>
2019-01-29 05:04:59 -0800

    add edge cases to delete party[Finishes #163501932]

--- src/controllers/partyCtr.js
+++ src/controllers/partyCtr.js
@@ -3,6 +3,8 @@ import uuid from 'uuid';
 import partyDb from '../db/partydb';
 import PartyModel from '../models/party';
 
+const partyModel = new PartyModel()
+
 class Party{
   /**
    * 
@@ -22,7 +24,10 @@ class Party{
       hqaddress,
       logoURL
     });
-    return res.status(201).json(partyDb);
+    return res.status(201).json({
+      "status": 201,
+      "data": partyDb
+    });
   }
   /**
    * 
@@ -35,11 +40,14 @@ class Party{
     let partyObject;
     partyDb.forEach((party) => {
       
-      if(party.id === partyId) {
+      if(party.id === Number(partyId)) {
         partyObject = party;
       }
     });
-    return res.status(200).json(partyObject);
+    return res.status(200).json({
+      "status": 200,
+      "data": partyObject
+    });
   }
   /**
    * 
@@ -48,7 +56,10 @@ class Party{
    * @returns {array} - returns all key value pairs as object in array
    */
   static getParties(req, res)  {
-    return res.status(200).json(partyDb);
+    return res.status(200).json({
+      "status": 200,
+      "data": partyDb
+    });
   }
 /**
    * 
@@ -57,27 +68,40 @@ class Party{
    * @returns {object} updated party
    */
   static update(req, res) {
-    const party = PartyModel.findOne(req.params.id);
+    let partyObject;
+
+    const party = partyModel.findOne(req.params.id);
     console.log(party);
     if (!party) {
-      return res.status(404).send({'message': 'party not found'});
+      return res.status(404).send({
+        "status": 404,
+        "error": "party not found"
+      });
     }
-    const updatedParty = PartyModel.update(req.params.id, req.body)
-    return res.status(200).send(updatedParty);
+    party.name = req.body.name
+    // const updatedParty = PartyModel.update(req.params.id, req.body)
+    return res.status(200).send(party);
   }
   /**
    * 
    * @param {object} req 
    * @param {object} res 
-   * @returns {void} return statuc code 204 
+   * @returns {void} return code 204 
    */
   static delete(req, res) {
-    const party = PartyModel.findOne(req.params.id);
+    const party = partyModel.findOne(req.params.id);
     if (!party) {
-      return res.status(404).send({'message': 'reflection not found'});
+      return res.status(404).send({
+        "status": 404,
+        "error": "party not found"
+      });
     }
-    const ref = PartyModel.delete(req.params.id);
-    return res.status(204).send(ref);
+    const ref = partyModel.delete(req.params.id);
+    return res.status(200).send({
+      "status": 200,
+      "message": "Party had been deleted",
+      "data": party
+    });
   }
 } 
 export default Party;
