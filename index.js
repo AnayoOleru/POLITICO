dd69956f25236bfb4eddf2cb131f460793b35de4
Anayo Oleru <anayo_oleru@outlook.com>
2019-01-29 06:53:28 -0800

    add default route

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ const port = process.env.PORT || 3000;
 
 
 app.get('/api/v1', (req, res) => res.status(200).send({
-  message: 'Welcome to POLITICO'
+  "status": 200,
+  "message": 'Welcome to POLITICO'
 }));
 app.post('/api/v1/parties', Party.createParty);
 app.get('/api/v1/parties/:partyId', Party.getPartyById);
@@ -25,6 +26,10 @@ app.delete('/api/v1/party/:id', Party.delete);
 app.post('/api/v1/office', Office.createOffice);
 app.get('/api/v1/office', Office.getOffice);
 app.get('/api/v1/office/:officeId', Office.getOfficeById);
+app.get('/', (req, res) => res.status(404).send({
+  "status": 404,
+  "message": "Page not found, your URL is incorrect"
+}));
 
 // app.listen(3000)
 // console.log('app running on port ', 3000);
