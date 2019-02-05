d34849feeca97a268e7dfca489c53fe113a91b28
Anayo Oleru <anayo_oleru@outlook.com>
2019-02-05 12:12:16 -0800

    admin should create office[#163739828 FInishes]

--- UI/scripts/govOffice.js
+++ UI/scripts/govOffice.js
@@ -16,3 +16,33 @@ function openNav() {
 function closeNav() {
     document.getElementById("mySidenav").style.width = "0";
 }
+
+
+// Consuming the API
+// alert("connected!");
+document.getElementById('addOffice').addEventListener('submit', addOffice);
+
+function addOffice(e){
+    e.preventDefault();
+
+    
+    let type = document.getElementById('type').value;
+    let name = document.getElementById('name').value;
+
+
+    fetch('http://localhost:3000/api/v1/offices', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            type: type, 
+            name: name
+        })
+        
+    })
+    .then((res) => res.json())
+    // render the office page
+        .then((res) => res.render('/views/parties.html'))
+}
