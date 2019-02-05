6f624e565c741f211adb10b396ee3ce9b90c1f66
Anayo Oleru <anayo_oleru@outlook.com>
2019-02-05 11:42:34 -0800

    user should login from the frontend[#163725462 Finishes]

---
+++ UI/scripts/signin.js
@@ -0,0 +1,27 @@
+// alert("connected!");
+document.getElementById('loginPost').addEventListener('submit', loginPost);
+
+function loginPost(e){
+    e.preventDefault();
+
+    
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+
+    fetch('http://localhost:3000/api/v1/auth/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email, 
+            password: password
+        })
+        
+    })
+    .then((res) => res.json())
+    // render user dashboard
+        .then((res) => res.render('/views/parties.html'))
+}
