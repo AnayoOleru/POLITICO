8a9c03e7ae07c82d3fdd112f58a5ccaf76c6c6a7
Anayo Oleru <anayo_oleru@outlook.com>
2019-05-25 11:26:01 +0100

    develop

--- UI/scripts/addparties.js
+++ UI/scripts/addparties.js
@@ -105,8 +105,8 @@ function addParty(e) {
       }
     });
 }
-const username;
-const nameside;
+// const username;
+// const nameside;
 function getParties() {
   fetch('https://trustpolitico.herokuapp.com/api/v1/parties', {
     method: 'GET',
@@ -145,9 +145,9 @@ function getParties() {
                 </div>
             </div> `;
 
-        username = `<li><a href="#" class="active">${payload.userName} ${payload.lastName}</a></li>`;
+        // username = `<li><a href="#" class="active">${payload.userName} ${payload.lastName}</a></li>`;
 
-        nameside = `<span>${payload.userName} ${payload.lastName}</span>`;
+        // nameside = `<span>${payload.userName} ${payload.lastName}</span>`;
       });
       document.getElementById('partyResult').innerHTML = result;
       document.getElementById('username').innerHTML = username;
